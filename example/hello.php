4a003004142fad4a47d7458d7298eaec2da37254
Igor Wiedler <igor@wiedler.ch>
2011-12-28 14:43:45 +0100

    add hello example

---
+++ example/hello.php
@@ -0,0 +1,16 @@
+<?php
+
+use Mongrel2\Connection;
+
+$sender_id = "82209006-86FF-4982-B5EA-D1E29E55D481";
+$conn = new Connection($sender_id, "tcp://127.0.0.1:9997", "tcp://127.0.0.1:9996");
+
+while (true) {
+    $req = $conn->recv();
+
+    if ($req->is_disconnect()) {
+        continue;
+    }
+
+    $this->conn->reply_http($req, 'Hello World');
+}
