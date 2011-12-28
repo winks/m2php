91fba1e01cc8bafdc00dcf718baa206337e6c016
Igor Wiedler <igor@wiedler.ch>
2011-12-28 02:41:23 +0100

    cs adjustments

--- src/Mongrel2/Request.php
+++ src/Mongrel2/Request.php
@@ -1,7 +1,8 @@
 <?php
 namespace m2php;
 
-class Request{
+class Request
+{
     public $sender;
     public $path;
     public $conn_id;
@@ -9,7 +10,8 @@ class Request{
     public $body;
     public $data;
 
-    public function __construct($sender, $conn_id, $path, $headers, $body) {
+    public function __construct($sender, $conn_id, $path, $headers, $body)
+    {
         $this->sender = $sender;
         $this->path = $path;
         $this->conn_id = $conn_id;
@@ -23,7 +25,8 @@ class Request{
         }
     }
 
-    public static function parse($msg) {
+    public static function parse($msg)
+    {
         list($sender, $conn_id, $path, $rest) = explode(' ', $msg, 4);
         $hd = \m2php\parse_netstring($rest);
         $headers = $hd[0];
@@ -36,7 +39,8 @@ class Request{
         return new \m2php\Request($sender, $conn_id, $path, $headers, $body);
     }
 
-    public function is_disconnect() {
+    public function is_disconnect()
+    {
         if ($this->headers->METHOD == 'JSON') {
             return $this->data['type'] == 'disconnect';
         }
--- src/Mongrel2/Tool.php
+++ src/Mongrel2/Tool.php
@@ -1,26 +1,31 @@
 <?php
 namespace m2php;
 
-function parse_netstring($ns) {
-    list($len, $rest) = explode(':', $ns, 2);
-    $len = intval($len);
+class Tool
+{
+    public function parse_netstring($ns)
+    {
+        list($len, $rest) = explode(':', $ns, 2);
+        $len = intval($len);
 
-    return array(
-        substr($rest, 0, $len),
-        substr($rest, $len+1)
-    );
-}
-
-function http_response($body, $code, $status, $headers) {
-    $http = "HTTP/1.1 %s %s\r\n%s\r\n%s";
-
-    if (is_null($headers)) {
-        $headers = array();
+        return array(
+            substr($rest, 0, $len),
+            substr($rest, $len+1)
+        );
     }
-    $headers['Content-Length'] = strlen($body);
-    $hd = "";
-    foreach($headers as $k => $v) {
-        $hd .= sprintf("%s: %s\r\n", $k, $v);
+
+    public function http_response($body, $code, $status, $headers)
+    {
+        $http = "HTTP/1.1 %s %s\r\n%s\r\n%s";
+
+        if (is_null($headers)) {
+            $headers = array();
+        }
+        $headers['Content-Length'] = strlen($body);
+        $hd = "";
+        foreach($headers as $k => $v) {
+            $hd .= sprintf("%s: %s\r\n", $k, $v);
+        }
+        return sprintf($http, $code, $status, $hd, $body);
     }
-    return sprintf($http, $code, $status, $hd, $body);
 }
