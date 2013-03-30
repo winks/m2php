06b2544f82b80fb5eee6f6a2cee40ca9d67ec5a3
Liam Zdenek <liam@vubeology.com>
2013-03-30 18:38:29 -0500

    Updating the readme and the example script to use phar's new installer and autoloader

--- example/hello.php
+++ example/hello.php
@@ -2,7 +2,7 @@
 
 use Mongrel2\Connection;
 
-require __DIR__.'/../vendor/.composer/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $sender_id = "82209006-86FF-4982-B5EA-D1E29E55D481";
 $conn = new Connection($sender_id, "tcp://127.0.0.1:9997", "tcp://127.0.0.1:9996");
